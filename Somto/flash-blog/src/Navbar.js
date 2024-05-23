79b76832a193654851a833dc9be695523497f4bd
sholanke precious <sholankeprecious3@gmail.com>
2024-05-23 16:24:21 +0100

    first commit

---
+++ Somto/flash-blog/src/Navbar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return (
+        <nav className="navbar">
+            <h1>the flash blog</h1>
+            <div className="links">
+                <a href="/">home</a>
+                <a href="/create">new blog</a>
+            </div>
+        </nav>
+    );
+}
+  
+  export default NavBar;
